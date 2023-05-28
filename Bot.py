3f29123370515e113cccdc5f2e57c388f1c20a86
leoco <leo.coelhoferreira@gmail.com>
2023-05-28 16:04:59 -0300

    1 commit

---
+++ Bot.py
@@ -0,0 +1,11 @@
+import chromedriver_binary
+from selenium import webdriver
+import time
+
+class roboYoutube():
+	def __init__(self):
+		self.webdriver = webdriver.Chrome()
+		
+Bot = roboYoutube()
+
+print("messing with git")
